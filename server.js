3150662dc4b5074d35cad96c724a6113cfe97fe2
janeokumbe <janeokumbe@gmail.com>
2024-10-07 22:33:56 +0300

    week 5 assignment

--- server.js
+++ server.js
@@ -1,87 +1,78 @@
 const express = require('express')
-const app = express();
+const mysql = require('mysql2')
+const dotenv = require('dotenv')
 
-const mysql = require('mysql2');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const app = express()
+dotenv.config()
 
-app.use(express.json());
-app.use(cors());
-dotenv.config();
-
+// create connection
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-
-});
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+})
 
 db.connect((err) => {
-    if(err) return console.log("Error connecting to MySQL");
-    console.log("connected to Mysql as id.", db.threadId);
-});
+  if(err) {
+    return console.log("Error connecting to MySQL", err)
+  }
+    console.log("MySQL connection successful")
+})
 
-app.set('view engine', "ejs")
-app.set('views', __dirname + "/views");
-
-app.get('/data', (req,res) =>{
-    // question 1: Retrieve all patients
-    db.query('SELECT * FROM patients', (err,results) =>{
-        if(err){
-            console.error(err);
-            res.status(500).send('error retrieving data')
-        }else {
-            res.render('data',{results: results});
-        }
-    });
-});
-
-app.get('/data', (req,res) =>{
-// question 2: Retrieve all providers
-db.query('SELECT * FROM providers', (err,results) =>{
+// question 1: retrieve patients
+app.get('/get-patients', (req, res) => {
+  const getPatients = "SELECT * FROM patients"
+  db.query(getPatients, (err, results) =>{
     if(err){
-        console.error(err);
-        res.status(500).send('error retrieving data')
-    }else {
-        res.render('data',{results: results});
+      return res.status(500).send("failed to retrieve patients")
     }
+    res.status(200).send(results)
   });
 });
 
-app.get('/data', (req, res) => {
-      
-    // Question 3: filter patients by first name
-    db.query('SELECT * FROM patients ORDER BY first_name', (err, results) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error filtering data');
-      } else {
-        res.render('data', { results: results });
+// question 2: Retrieve all providers
+app.get('/get-providers', (req,res) => {
+  const getproviders = "SELECT * FROM providers"
+  db.query(getproviders, (err,results) =>{
+  if(err){
+          return res.status(500).send('error retrieving data')
       }
+        res.status(200).send(results);
     });
   });
 
-  app.get('/data', (req, res) => {
+  // question 3 :Filter patients by first_name
+  app.get('/filter-patients', (req, res) => {
+        const getpatients= "SELECT * FROM patients ORDER BY first_name"
+    db.query(getpatients, (err, results) => {
+          if (err) {
+            return res.status(500).send('Error filtering data');
+          } 
+            res.status(200).send(results);
+         
+        });
+      });
+
+      // Question 4: Retrieve all providers by their specialty
+      app.get('/provider-specialty', (req, res) => {
+          const getproviders = "SELECT * FROM providers ORDER BY provider_specialty" 
+        db.query(getproviders, (err, results) => {
+              if (err) {
+                return res.status(500).send('Error retrieving data');
+              } else {
+                res.status(200).send(results);
+              }
+            });
+          });
+
+
       
-    // Question 4: Retrieve all providers by their specialty
-    db.query('SELECT * FROM providers ORDER BY provider_specialty', (err, results) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error retrieving data');
-      } else {
-        res.render('data', { results: results });
-      }
-    });
-  });
-  
 
-const PORT = 3300
-app.listen(process.env.PORT, () => {
-  console.log(`server is running on port ${process.env.PORT}`);
 
-  console.log('sending message to browser')
-  app.get('/',(req,res) => {
-    res.send('server started successfully');
-  });
-});
+
+
+const PORT = 3300;
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`)
+})
